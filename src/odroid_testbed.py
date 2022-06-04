a776f657b4f074b1a519762c10558dacfeacf80c
smustafauludag <smustafauludag@gmail.com>
2022-06-04 12:27:18 +0300

    .

--- src/odroid_testbed.py
+++ src/odroid_testbed.py
@@ -18,9 +18,10 @@ def main():
   
   try:
     uav.SetMode("GUIDED")
+    uav.nav.ArmDisarm(1)
     #uav.Go2MissionPoint("MEDIC_1")
-    uav.Takeoff(2)
-    uav.nav.SetSpeedLocalNed(0.5,0,0,0)
+    #uav.Takeoff(2)
+    #uav.nav.SetSpeedLocalNed(0.5,0,0,0)
 
 
   except KeyboardInterrupt:
